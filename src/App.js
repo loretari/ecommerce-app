f376a2b6f973f9d8f4bfb4fb6c8e9a331317ef8e
loretari <loretari@gmail.com>
2023-11-07 12:48:40 +0200

    init: initial first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
+import Home from "./pages/Home";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-   <h1>Hello from...</h1>
+      <>
+        <Routes>
+         <Route path = "/" element = { <Home/> }/>
+          <Route path = "*" element = { <Navigate />}/>
+        </Routes>
+        </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import App from './App';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 
 root.render(
+<React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+</React.StrictMode>
 
-      <App />
 
 );
 
