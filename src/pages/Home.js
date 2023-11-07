39d823ef182ef4713c3927455f9047f6399195ce
loretari <loretari@gmail.com>
2023-11-07 21:15:24 +0200

    init: add Hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Layout from "./Layout";
+import Hero from "../Components/Hero/Hero";
+
 
         const Home =() => {
     return (
         <div>
-            <Layout/>
+            <Hero />
         </div>
     )
         }
