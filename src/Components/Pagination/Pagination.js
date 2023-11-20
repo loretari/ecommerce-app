385ef23a80b85d73c14f1d6299f450c437dbf0d5
loretari <loretari@gmail.com>
2023-11-20 16:19:30 +0200

    init: fix bug

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -10,13 +10,13 @@ function MyPagination () {
               <nav className= "paginationTop" >
                  <ul className="pagination ">
                      <li className="page-item disabled">
-                        <a className="page-link" href="shop" tabIndex="-1">Previous</a>
+                        <a className="page-link" href="/shop" tabIndex="-1">Previous</a>
                     </li>
-                     <li className="page-item"><a className="page-link" href="shop">1</a></li>
-                       <li className="page-item"><a className="page-link" href="shop">2</a></li>
-                    <li className="page-item"><a className="page-link" href="shop">3</a></li>
+                     <li className="page-item"><a className="page-link" href="/shop">1</a></li>
+                       <li className="page-item"><a className="page-link" href="/shop">2</a></li>
+                    <li className="page-item"><a className="page-link" href="/shop">3</a></li>
                      <li className="page-item">
-                         <a className="page-link" href="shop">Next</a>
+                         <a className="page-link" href="/shop">Next</a>
                     </li>
                  </ul>
                 </nav>
