44f3a320a067c5a0b56da0532da7084466ff2caf
loretari <loretari@gmail.com>
2023-11-20 12:38:48 +0200

    init: fix some bugs

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -12,11 +12,11 @@ function MyPagination () {
                      <li className="page-item disabled">
                         <a className="page-link" href="shop" tabIndex="-1">Previous</a>
                     </li>
-                     <li className="page-item"><a className="page-link" href="#">1</a></li>
-                       <li className="page-item"><a className="page-link" href="#">2</a></li>
-                    <li className="page-item"><a className="page-link" href="#">3</a></li>
+                     <li className="page-item"><a className="page-link" href="shop">1</a></li>
+                       <li className="page-item"><a className="page-link" href="shop">2</a></li>
+                    <li className="page-item"><a className="page-link" href="shop">3</a></li>
                      <li className="page-item">
-                         <a className="page-link" href="#">Next</a>
+                         <a className="page-link" href="shop">Next</a>
                     </li>
                  </ul>
                 </nav>
